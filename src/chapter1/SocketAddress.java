c6d8dfec49dc399addd8ee92648a9b82121e38c5
hadesyi <leesk55@gmail.com>
2019-04-07 03:54:23 +0900

    Socket Address

--- src/chapter1/SocketAddress.java
+++ src/chapter1/SocketAddress.java
@@ -5,13 +5,11 @@ import java.net.InetSocketAddress;
 public class SocketAddress {
 
     public static void main(String[] args) {
-        public static void main (String[]args){
-            InetSocketAddress addr1 = new InetSocketAddress("::1", 12345);
-            printSocketAddress(addr1);
+        InetSocketAddress addr1 = new InetSocketAddress("::1", 12345);
+        printSocketAddress(addr1);
 
-            InetSocketAddress addr2 = InetSocketAddress.createUnresolved("::1",12881);
-            printSocketAddress(addr2);
-        }
+        InetSocketAddress addr2 = InetSocketAddress.createUnresolved("::1", 12881);
+        printSocketAddress(addr2);
     }
 
     public static void printSocketAddress(InetSocketAddress sAddr) {
